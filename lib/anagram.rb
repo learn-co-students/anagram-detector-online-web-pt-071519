9a584a7c2d7d66d1b26c50153ae5de9acbc17176
Jenny O'Hara <jenny.s.ohara@gmail.com>
2019-07-31 14:29:20 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+
+      #iterate over an array and check if any words match the word initialised with Anagram.new
+      #we need to split the array_word into letters, sort them into order, and compare against @word
+      #add any matches into the matches array
+      #finally return the matches array, which will either have one or more matches, or be an empty array
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end  
+  
+  def match(array)
+    array.keep_if do |str| 
+     str.split('').sort == @word.split('').sort 
+    end
+  end 
+end   
