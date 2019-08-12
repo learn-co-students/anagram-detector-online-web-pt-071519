11a049aef6a8fd79a02ba7cd5297feba12afce0b
LaNice Powell <lanicemp@aol.com>
2019-08-12 13:45:42 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,29 +1,23 @@
-# Your code goes here!
 require "pry"
 
 class Anagram 
+  attr_accessor :name 
   
-  attr_accessor :word 
-  
-  def initialize(word)
-    @word = word 
-    #binding.pry
+  def initialize(name)
+    @name = name  
+    #binding.pry 
   end 
   
   def match(array)
     matches = []
     array.each do |word|
-     if   word.split('').sort == self.word.split('').sort
+      if word.split('').sort == self.name.split('').sort
        matches << word 
-       
+       #binding.pry
      #matches << word if word.split('').sort == self.word.split('').sort (this code also works and is a simpler refactored method )
       
-     #binding.pry
-   end 
-   end 
-   matches 
+      end 
     end 
-    
-
-
+   matches 
+  end 
 end 
