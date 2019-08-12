150d600c94e436eff20bd284d6d393f139a98981
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-08-12 14:57:15 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :anagram 
+  
+ def initialize(word)
+   @word = word
+ end
+  
+  def match(word)
+    word.select do |word|
+      word.split("").sort == @word.split("").sort
+    end
+  end
+  
+  
+  
+  
+  
+  
+end
