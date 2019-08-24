28af0b560ed903c39e600c08a7e0c010c7f55c7e
Winston  Chung <18.chung.winston@ctkprep.org>
2019-08-24 23:13:42 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Anagram
+ 
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    word_splitter = @word.split("").sort
+    new_array = []
+    for each in array
+      if each.split("").sort == word_splitter
+        new_array << each
+      end
+    end
+    new_array
+  end
+end
