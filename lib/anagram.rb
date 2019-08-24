8a65cd2d5201ff01751e78e67d21927fd72e59c6
Broderick  Fowler Jr <broderickfowler@outlook.com>
2019-08-24 23:34:19 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+end
+
+  def match(array_of_words)
+    array_of_words.select do |e|
+      (@word.split("").sort) == (e.split("").sort)
+    end
+end
+  
+  
+  
+end
